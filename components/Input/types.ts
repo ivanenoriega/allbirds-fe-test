a8f6441982a4ec0227402e6d13b686c18ee19368
Ivan Noriega <ivanenoriega@gmail.com>
2022-11-01 08:14:31 -0300

    add Input component

---
+++ components/Input/types.ts
@@ -0,0 +1,35 @@
+type InputTypes =
+  | 'button'
+  | 'checkbox'
+  | 'color'
+  | 'date'
+  | 'datetime-local'
+  | 'email'
+  | 'file'
+  | 'hidden'
+  | 'image'
+  | 'month'
+  | 'number'
+  | 'password'
+  | 'radio'
+  | 'range'
+  | 'reset'
+  | 'search'
+  | 'submit'
+  | 'tel'
+  | 'text'
+  | 'time'
+  | 'url'
+  | 'week';
+
+interface inputProps {
+    type?: InputTypes
+    value: string
+    name: string
+    label?: string
+    id?: string
+    maxlength?: number
+    onChange?: (e?: any) => void
+}
+
+export default inputProps
