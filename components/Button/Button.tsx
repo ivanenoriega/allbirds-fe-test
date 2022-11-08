b288805d507c28ea4d47a5de861c4813c594daae
Ivan Noriega <ivanenoriega@gmail.com>
2022-11-08 10:52:12 -0300

    new button variants

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,13 +2,20 @@ import { FC } from "react";
 import buttonProps from "./Button.types";
 import styles from "./Button.module.scss";
 import classNames from "classnames";
-import { BUTTON_TYPE_BUTTON } from "./Button.constants";
+import { BUTTON_TYPE_BUTTON, BUTTON_VARIANT_PRIMARY } from "./Button.constants";
 
-const Button: FC<buttonProps> = ({ children, type = BUTTON_TYPE_BUTTON, disabled = false }) => {
+const Button: FC<buttonProps> = ({
+    type = BUTTON_TYPE_BUTTON,
+    disabled = false,
+    onClick = undefined,
+    variant = BUTTON_VARIANT_PRIMARY,
+    children,
+}) => {
     return <button
         disabled={disabled}
         type={type}
-        className={classNames(styles.button, { [styles.disabled]: disabled })}
+        onClick={onClick}
+        className={classNames(styles.button, { [styles.disabled]: disabled, [styles[variant]]: variant })}
     >{children}</button>;
 };
 
--- components/Button/Button.types.ts
+++ components/Button/Button.types.ts
@@ -1,5 +1,11 @@
 import { ReactNode } from "../../types";
-import { BUTTON_TYPE_BUTTON, BUTTON_TYPE_RESET, BUTTON_TYPE_SUBMIT } from "./Button.constants";
+import {
+    BUTTON_TYPE_BUTTON,
+    BUTTON_TYPE_RESET,
+    BUTTON_TYPE_SUBMIT,
+    BUTTON_VARIANT_PRIMARY,
+    BUTTON_VARIANT_SECONDARY
+} from "./Button.constants";
 
 type buttonTypes = typeof BUTTON_TYPE_SUBMIT | typeof BUTTON_TYPE_BUTTON | typeof BUTTON_TYPE_RESET;
 
@@ -7,6 +13,8 @@ interface buttonProps {
     children: ReactNode
     type?: buttonTypes
     disabled?: boolean
+    variant?: typeof BUTTON_VARIANT_PRIMARY | typeof BUTTON_VARIANT_SECONDARY
+    onClick?: (e?: any) => void
 }
 
 export default buttonProps;
