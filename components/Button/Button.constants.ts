95d868efbf14de56c8d3bbd998d17c92c13a0837
Ivan Noriega <ivanenoriega@gmail.com>
2022-11-08 10:56:35 -0300

    fix minor issue

--- components/Button/Button.constants.ts
+++ components/Button/Button.constants.ts
@@ -1,3 +1,5 @@
 export const BUTTON_TYPE_SUBMIT = 'submit';
 export const BUTTON_TYPE_BUTTON = 'button';
 export const BUTTON_TYPE_RESET = 'reset';
+export const BUTTON_VARIANT_PRIMARY = 'primary';
+export const BUTTON_VARIANT_SECONDARY = 'secondary';
