b0188f4834d3f0a0a82dd0d3796b45fb36a017e6
Ivan Noriega <ivanenoriega@gmail.com>
2022-11-08 16:52:18 -0300

    update test

--- components/RegistrationForm/RegistrationForm.test.js
+++ components/RegistrationForm/RegistrationForm.test.js
@@ -3,6 +3,9 @@ import '@testing-library/jest-dom';
 import RegistrationForm from './RegistrationForm';
 
 let container;
+const getFirstNameInput = () => screen.getByRole('textbox', {
+  name: /first name/i
+});
 
 describe('RegistrationForm', () => {
   beforeEach(() => {
@@ -64,23 +67,81 @@ describe('RegistrationForm', () => {
     });
   });
   describe('input validations', () => {
-    it('first name - happy path', () => {
-      const input = screen.getByRole('textbox', {
-        name: /first name/i
+    describe('first name', () => {
+      it('happy path', () => {
+        const input = getFirstNameInput();
+        expect(input.value).toBe('');
+        fireEvent.change(input, { target: { value: 'valid text value' } });
+        expect(input.value).toBe('valid text value');
       });
-      expect(input.value).toBe('');
-      fireEvent.change(input, { target: { value: 'valid text value' } });
-      expect(input.value).toBe('valid text value');
+      it('invalid characters', () => {
+        const input = getFirstNameInput();
+        expect(input.value).toBe('');
+        fireEvent.change(input, { target: { value: '!invalid text value' } });
+        expect(input.value).toBe('!invalid text value');
+        const errorMessage = screen.getByText(/first name cannot contain special characters\./i);
+        expect(errorMessage).toBeInTheDocument();
+      });
     });
-    it('first name - invalid characters', () => {
-      const input = screen.getByRole('textbox', {
-        name: /first name/i
+    describe('last name', () => {
+      it('happy path', () => {
+        const input = screen.getByRole('textbox', {
+          name: /last name/i
+        });
+        expect(input.value).toBe('');
+        fireEvent.change(input, { target: { value: 'valid text value' } });
+        expect(input.value).toBe('valid text value');
       });
-      expect(input.value).toBe('');
-      fireEvent.change(input, { target: { value: '!invalid text value' } });
-      expect(input.value).toBe('!invalid text value');
-      const errorMessage = screen.getByText(/first name cannot contain special characters\./i);
-      expect(errorMessage).toBeInTheDocument();
+      it('invalid characters', () => {
+        const input = screen.getByRole('textbox', {
+          name: /last name/i
+        });
+        expect(input.value).toBe('');
+        fireEvent.change(input, { target: { value: '!invalid text value' } });
+        expect(input.value).toBe('!invalid text value');
+        const errorMessage = screen.getByText(/last name cannot contain special characters\./i);
+        expect(errorMessage).toBeInTheDocument();
+      });
+    });
+    describe('phone number', () => {
+      it('happy path', () => {
+        const input = screen.getByRole('textbox', {
+          name: /phone number/i
+        });
+        expect(input.value).toBe('');
+        fireEvent.change(input, { target: { value: '1234567890' } });
+        expect(input.value).toBe('(123) 456-7890');
+      });
+      it('invalid format', () => {
+        const input = screen.getByRole('textbox', {
+          name: /phone number/i
+        });
+        expect(input.value).toBe('');
+        fireEvent.change(input, { target: { value: '12345' } });
+        expect(input.value).toBe('(123) 45');
+        const errorMessage = screen.getByText(/phone number has wrong format\. ex: \(123\) 456\-7890\./i);
+        expect(errorMessage).toBeInTheDocument();
+      });
+    });
+    describe('email', () => {
+      it('happy path', () => {
+        const input = screen.getByRole('textbox', {
+          name: /email/i
+        });
+        expect(input.value).toBe('');
+        fireEvent.change(input, { target: { value: 'test@test.com' } });
+        expect(input.value).toBe('test@test.com');
+      });
+      it('invalid format', () => {
+        const input = screen.getByRole('textbox', {
+          name: /email/i
+        });
+        expect(input.value).toBe('');
+        fireEvent.change(input, { target: { value: 'invalid email' } });
+        expect(input.value).toBe('invalid email');
+        const errorMessage = screen.getByText(/email has invalid format\. ex: user@company\.com/i);
+        expect(errorMessage).toBeInTheDocument();
+      });
     });
   });
 });
